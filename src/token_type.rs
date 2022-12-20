c885522f5fe600d015916b11f860f7e92c5122f7
Dylan Toth <dylant1@yahoo.com>
2022-12-20 15:18:59 -0600

    Update token type

--- src/token_type.rs
+++ src/token_type.rs
@@ -1,21 +1,22 @@
 
 pub enum TokenType {
     //Single character tokens
-    LEFT_PAREN, RIGHT_PAREN, LEFT_BRACE, RIGHT_BRACE,
-    COMMA, DOT, MINUS, PLUS, SEMICOLON, SLASH, STAR,
+    LeftParen, RightParen, LeftBrace, RightBrace,
+    Comma, Dot, Minus, Plus, Semicolon, Slash, Star,
 
     //One or two character tokens
-    BANG, BANG_EQUAL,
-    EQUAL, EQUAL_EQUAL,
-    GREATER, GREATER_EQUAL,
-    LESS, LESS_EQUAL,
+    Bang, BangEqual,
+    Equal, EqualEqual,
+    Greater, GreaterEqual,
+    Less, LessEqual,
 
-    //Literals
-    IDENTIFIER, STRING, NUMBER,
+    //literals
+    Identifier, String, Number,
 
     //keywords
-    AND, CLASS, ELSE, FALSE, FUN, FOR, IF, NIL, OR,
-    PRINT, RETURN, SUPER, THIS, TRUE, VAR, WHILE,
-    
-    EOF
+    And, Class, Else, False, Fun, For, If, Nil, Or,
+    Print, Return, Super, This, True, Var, While,
+
+    Eof
+
 }
